0c519ab42e8092d89d2e2aa6336577c52f8868c5
Pertwy <johnpatrickperkins@gmail.com>
2021-03-29 22:32:52 +0100

    military service added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -60,6 +60,16 @@ export default function Profile(){
   const [ educationEndDate, setEducationEndDate] =useState("");
   const [ educationCollegeOrUniversity, setEducationCollegeOrUniversity] =useState("");
 
+  const [expandMilitaryService, setExpandMilitaryService] = useState(false)
+  const [militaryServiceCountry, setMilitaryServiceCountry] = useState("")
+  const [militaryServiceUnit, setMilitaryServiceUnit] = useState("")
+  const [militaryRank, setMilitaryRank] = useState("")
+  const [militaryServiceStartDate, setMilitaryServiceStartDate] = useState("")
+  const [militaryServiceEndDate, setMilitaryServiceEndDate] = useState("")
+  const [militaryServiceDescription, setMilitaryServiceDescription] = useState("")
+  const [militaryServiceComendations, setMilitaryServiceComendations] = useState("")
+
+
   const [expandSkills, setExpandSkills] = useState(false)
   const [newSkill, setNewSkill] = useState("")
 
@@ -179,13 +189,13 @@ function handleSaveWorkExperience(){
     description:WEDescription
   })
   setUserData({...userData, workExperience:newWorkExperience})
-  setExpandWorkExperience(!expandWorkExperience)
+  setExpandWorkExperience(false)
 }
 
 function handleDeleteWorkExperience(prop){
   const workExperience = userData.workExperience.filter(workExperience => workExperience.jobTitle !== prop);
   setUserData({...userData, workExperience:workExperience})
-  setExpandWorkExperience(!expandWorkExperience)
+  setExpandWorkExperience(false)
 }
 
 
@@ -317,13 +327,13 @@ function handleSaveEducation(){
   })
 
   setUserData({...userData, education: newEducation})
-  setExpandEducation(!expandEducation)
+  setExpandEducation(false)
 }
 
 function handleDeleteEducation(prop){
   const education = userData.education.filter(education => education.levelOfEducation !== prop);
   setUserData({...userData, education:education})
-  setExpandEducation(!expandEducation)
+  setExpandEducation(false)
 }
 
 
@@ -433,6 +443,153 @@ if(userData.education.length > 0 && !expandEducation){
     }
 
     
+
+
+
+//Military Service ///////////////////////////////////////////////
+function handleExpandMilitaryService(){
+  setExpandMilitaryService(!expandMilitaryService)
+}
+
+function handleSaveMilitaryService(){
+  let newMilitaryService = userData.militaryService
+  newMilitaryService.push({
+    country: militaryServiceCountry,
+    unit: militaryServiceUnit,
+    rank: militaryRank,
+    startDate: militaryServiceStartDate,
+    endDate: militaryServiceEndDate,
+    description: militaryServiceDescription,
+    comendations: militaryServiceComendations
+  })
+
+  setUserData({...userData, militaryService:newMilitaryService})
+  setExpandMilitaryService(false)
+}
+
+function handleDeleteMilitaryService(prop){
+  const militaryService = userData.militaryService.filter(militaryService => militaryService.jobTitle !== prop);
+  setUserData({...userData, militaryService:militaryService})
+  setExpandMilitaryService(false)
+}
+
+
+function handleCancelMilitaryService(){
+  setMilitaryServiceCountry("")
+  setMilitaryServiceUnit("")
+  setMilitaryRank("")
+  setMilitaryServiceStartDate("")
+  setMilitaryServiceEndDate("")
+  setMilitaryServiceDescription("")
+  setMilitaryServiceComendations("")
+  setExpandMilitaryService(!expandMilitaryService)
+}
+
+
+
+let MilitaryService
+if(userData.militaryService.length > 0 && !expandMilitaryService){
+  MilitaryService =
+    <>
+      <MilitaryServiceMap></MilitaryServiceMap>
+    </>
+  } else if(userData.militaryService.length > 0 && expandMilitaryService){
+    MilitaryService =
+    <>
+      <MilitaryServiceMap></MilitaryServiceMap>
+      <>
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceCountry(target.value)} id="standard-basic" label="Country" />
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceUnit(target.value)} id="standard-basic" label="Unit"/>
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryRank(target.value)} id="standard-basic" label="Rank" />
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceStartDate(target.value)} id="standard-basic" label="Start Date"/>
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceEndDate(target.value)} id="standard-basic" label="End Date"/>  
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceDescription(target.value)} id="standard-basic" label="Description"/>         
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceComendations(target.value)} id="standard-basic" label="Comendations"/>   
+
+        
+
+        <Button onClick={()=>handleSaveMilitaryService()} variant="outlined">
+          Save Changes
+        </Button>
+        <Button onClick={()=>handleCancelMilitaryService()} variant="outlined">
+          Cancel
+        </Button>
+      </>
+    </>
+  } else if(userData.militaryService.length == 0 && expandMilitaryService){
+    MilitaryService =
+      <>
+               <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceCountry(target.value)} id="standard-basic" label="Country" />
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceUnit(target.value)} id="standard-basic" label="Unit"/>
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryRank(target.value)} id="standard-basic" label="Rank" />
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceStartDate(target.value)} id="standard-basic" label="Start Date"/>
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceEndDate(target.value)} id="standard-basic" label="End Date"/>  
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceDescription(target.value)} id="standard-basic" label="Description"/>         
+
+        <TextField fullWidth  onChange={({ target }) =>     
+              setMilitaryServiceComendations(target.value)} id="standard-basic" label="Comendations"/>           
+
+        
+
+        <Button onClick={()=>handleSaveMilitaryService()} variant="outlined">
+          Save Changes
+        </Button>
+        <Button onClick={()=>handleCancelMilitaryService()} variant="outlined">
+          Cancel
+        </Button>
+   </>
+ 
+  } else{
+    MilitaryService = <></>
+  }
+
+
+    function MilitaryServiceMap(){
+      return userData.militaryService.map(MilitaryService => {
+        return (
+          <div className={"pl-3 pr-3"}>
+            <div className={"row space-between"}>
+              <h4>{MilitaryService.jobTitle}</h4>
+              <button onClick={()=>handleDeleteMilitaryService(MilitaryService.jobTitle)}>delete</button>
+            </div>
+            <p>Country: {MilitaryService.country}</p>
+            <p>Unit: {MilitaryService.unit}</p>
+            <p>Start Date: {MilitaryService.startDate}</p>
+            <p>End Date: {MilitaryService.endDate}</p>
+            <p>Rank: {MilitaryService.rank}</p>
+            <p>Description: {MilitaryService.description}</p>
+            <p>Comendations: {MilitaryService.comendations}</p>
+      
+            
+          </div>
+        )
+      })
+    }
+
 
 
 
@@ -757,8 +914,9 @@ if(userData.languages.length > 0 && !expandLanguages){
             <section className={"personal-details" }>
             <div className={"row space-between pl-3 pr-3 border-top-0 border-right-0 border border-left-0" }>
                 <h4> Military Service </h4>
-                <button onClick={() => handleAddLanguage()}> + </button>
+                <button onClick={() => handleExpandMilitaryService()}> + </button>
               </div>
+              {MilitaryService}
             </section>
 
 
