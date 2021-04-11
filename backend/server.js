e326a4f78f5b5f6444e33558a91a163a6899407e
Pertwy <johnpatrickperkins@gmail.com>
2021-04-11 18:46:45 +0100

    added build instructions to server.js

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,14 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
 })
 
+if (process.env.NODE_ENV === "production"){
+
+    app.use(express.static("build"))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../build", "index.html"))
+    })
+}
 
 
 const jobpostsRouter = require('./routes/jobPosts')
