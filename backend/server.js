390dd2041a5b135b61e0e9afa8bb993a5b5573fe
rameezamemon1 <rameeza.memon1@gmail.com>
2021-04-13 17:19:26 +0500

    Base url added

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,9 @@ connection.once('open', () => {
 
 if (process.env.NODE_ENV === "production"){
 
-    app.use(express.static("build"))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../build", "index.html"))
+    app.use(express.static(path.join(__dirname, '../build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../build'))
     })
 }
 
